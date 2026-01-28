111522cfd8e2c6b66fbc3aeab10cf47af9552236
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-01-28 11:43:12 -0600

    Mateo 16:18 'sobre esta roca edificaré mi iglesia'.

---
+++ src/app/Collections.java
@@ -0,0 +1,9 @@
+package app;
+public interface Collections<T> {
+
+    void push(T element);
+
+    T pop();
+
+    T peek();
+}
---
+++ src/app/Vector.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Vector implements Collections<T> {
+
+
+    @Override
+    public void push(T element) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+}
