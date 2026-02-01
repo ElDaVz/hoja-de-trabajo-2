eea4751a3e65b4ab03fe49d6deec0fa91822f774
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-01-31 21:40:48 -0600

    Feat: added new implementations for the reader class and Main class

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String res = Reader.readFile("C:\\Users\\Danri\\Documents\\datos.txt");
+        System.out.println(res);
+    }
+}
--- src/app/Reader.java
+++ src/app/Reader.java
@@ -1,8 +1,18 @@
 package app;
 import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Reader {
-    File file;
-    Scanner sc = new Scanner(file);
+
+    public static String readFile(String path) {
+        try (Scanner sc = new Scanner(new File(path))) {
+
+            return sc.nextLine();
+
+        } catch (FileNotFoundException e) {
+
+        }
+        return null;
+    }
 }
