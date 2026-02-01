cb88a5627561686cfd9025177b551e4c153592c8
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-01-31 23:27:51 -0600

    Feat: added new class StackVector implementing java.util.Vector library and some tests classes

---
+++ src/app/StackVector.java
@@ -0,0 +1,29 @@
+package app;
+import java.util.Vector;
+
+public class StackVector<T> implements Stack<T> {
+
+    private Vector<T> vector;
+
+    public StackVector() {
+        vector = new Vector<>();
+    }
+
+    @Override
+    public void push(T element) {
+        vector.add(element);
+    }
+
+    @Override
+    public T pop() {
+        var lastElement = vector.lastElement();
+        vector.remove(lastElement);
+
+        return lastElement;
+    }
+
+    @Override
+    public T peek() {
+        return vector.getLast();
+    }
+}
