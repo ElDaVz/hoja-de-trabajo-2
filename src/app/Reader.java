2916622b88f2478e8fa8ba5bbdc35a2f2ff62343
Jordi <88168924+JordiCardona7@users.noreply.github.com>
2026-02-02 18:53:38 -0600

    Fix: Class Reader now read all the lines from de txt doc

--- src/app/Reader.java
+++ src/app/Reader.java
@@ -1,18 +1,30 @@
 package app;
+
 import java.io.File;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Reader {
 
-    public static String readFile(String path) {
+    public static List<String> readFile(String path) {
+
+        List<String> lines = new ArrayList<>();
+
         try (Scanner sc = new Scanner(new File(path))) {
 
-            return sc.nextLine();
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                if (!line.isEmpty()) {
+                    lines.add(line);
+                }
+            }
 
         } catch (FileNotFoundException e) {
-
+            System.out.println("Archivo no encontrado");
         }
-        return null;
+
+        return lines;
     }
 }
