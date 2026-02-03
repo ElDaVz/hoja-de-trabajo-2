71d9f22ea01f263919c151111a62448a8d6d5251
Jordi <88168924+JordiCardona7@users.noreply.github.com>
2026-02-02 19:03:11 -0600

    Fix: implement main to read the postfix expressions from the file and evaluate them

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import app.Reader;
+import app.PostFixCalculator;
+import app.Calc;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        String res = Reader.readFile("C:\\Users\\Danri\\Documents\\datos.txt");
-        System.out.println(res);
+        Calc calculator = new PostFixCalculator();
+
+        for (String expr : Reader.readFile("C:\\Users\\Danri\\Documents\\datos.txt")) {
+            int result = calculator.operate(expr);
+            System.out.println(expr + " = " + result);
+        }
     }
 }
