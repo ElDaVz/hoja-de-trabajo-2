b9ea18cbd3827a97c5e874d2c184c68be13d495b
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-02-03 18:54:06 -0600

    Added new StackA which uses ArrayLists instead of Arrays

---
+++ src/app/StackA.java
@@ -0,0 +1,33 @@
+package app;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class StackA<T> implements Stack<T> {
+    private ArrayList<T> items;
+
+    public StackA(int capacity) {
+        items = new ArrayList<>(capacity);
+
+    }
+
+    @Override
+    public T peek() {
+        if (items.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return items.getLast();
+    }
+
+    @Override
+    public T pop() {
+        if (items.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return items.removeLast();
+    }
+
+    @Override
+    public void push(T element) {
+        items.add(element);
+    }
+}
